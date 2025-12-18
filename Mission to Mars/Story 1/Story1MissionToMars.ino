e289d1948c5d8c2d48664ca2edd898c6ec42a61f
PossumMonarch <Salakast@outlook.com>
2025-12-18 14:54:42 +0000

    Update Story1MissionToMars.ino

--- Mission to Mars/Story 1/Story1MissionToMars.ino
+++ Mission to Mars/Story 1/Story1MissionToMars.ino
@@ -5,8 +5,8 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(5000); // Wait ;for 1000 millisecond(s)
+  delay(5000); // Wait for 5000 millisecond(s)
   digitalWrite(LED_BUILTIN, LOW);
-  delay(5000); // Wait ;for 1000 millisecond(s)
+  delay(5000); // Wait for 5000 millisecond(s)
 
 }
