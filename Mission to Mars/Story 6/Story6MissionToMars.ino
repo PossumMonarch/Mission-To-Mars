b328accf9f8cc6cb1c96e6f186246814b07b6353
PossumMonarch <Salakast@outlook.com>
2026-01-14 15:59:51 +0000

    Add files via upload

---
+++ Mission to Mars/Story 6/Story6MissionToMars.ino
@@ -0,0 +1,55 @@
+const int RIGHT_FEEDBACK = 2;
+const int LEFT_FEEDBACK = 3;
+int LEFT_REV = 12;
+int LEFT_FWD = 11;
+int LEFT_ENABLE = 10;
+int RIGHT_ENABLE = 9;
+int RIGHT_FWD = 8;
+int RIGHT_REV = 7;
+volatile int leftcounter = 0;
+volatile int rightcounter = 0;
+
+void setup() {
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(LEFT_FEEDBACK), LeftMotorISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_FEEDBACK), RightMotorISR, RISING);
+}
+
+void forward(int delaytime)
+{
+digitalWrite (LEFT_REV, LOW);
+digitalWrite (RIGHT_REV, LOW);
+digitalWrite (LEFT_ENABLE, HIGH);
+digitalWrite (RIGHT_ENABLE, HIGH);
+digitalWrite (LEFT_FWD, HIGH);
+digitalWrite (RIGHT_FWD, HIGH);
+delay(delaytime);
+}
+
+void LeftMotorISR() {
+  leftcounter++;
+}
+
+void RightMotorISR() {
+  rightcounter++;
+}
+
+void loop() {
+Serial.print("Left Counter");
+Serial.println(leftcounter);
+
+Serial.print("Right Counter");
+Serial.println(rightcounter);
+
+forward(1000000000000000000000);
+
+if (leftcounter >= 870) {
+  exit(0);
+}
+
+if (rightcounter >= 870) {
+  exit(0);
+}
+}
+
+
