d05cd66a549ab3916a3d77b26bd7a815caa27142
PossumMonarch <Salakast@outlook.com>
2026-02-02 15:09:10 +0000

    Add files via upload

---
+++ Mission to Mars/Story7MissionToMars/Story7MissionToMars.ino
@@ -0,0 +1,71 @@
+int LEFT_REV = 12;
+int LEFT_FWD = 11;
+int LEFT_ENABLE = 10;
+int RIGHT_ENABLE = 9;
+int RIGHT_FWD = 8;
+int RIGHT_REV = 7;
+void setup()
+{
+  pinMode(LEFT_REV, OUTPUT);
+  pinMode(RIGHT_REV, OUTPUT);
+  pinMode(LEFT_ENABLE, OUTPUT);
+  pinMode(RIGHT_ENABLE, OUTPUT);
+  pinMode(LEFT_FWD, OUTPUT);
+  pinMode(RIGHT_FWD, OUTPUT);
+}
+
+void forward(int delaytime)
+{
+digitalWrite (LEFT_REV, LOW);
+digitalWrite (RIGHT_REV, LOW);
+digitalWrite (LEFT_ENABLE, HIGH);
+digitalWrite (RIGHT_ENABLE, HIGH);
+digitalWrite (LEFT_FWD, HIGH);
+digitalWrite (RIGHT_FWD, HIGH);
+delay(delaytime);
+}
+
+void turnleft(int delaytime)
+{
+digitalWrite (LEFT_REV, LOW);
+digitalWrite (RIGHT_REV, LOW);
+digitalWrite (LEFT_ENABLE, LOW);
+digitalWrite (RIGHT_ENABLE, HIGH);
+digitalWrite (LEFT_FWD, LOW);
+digitalWrite (RIGHT_FWD, HIGH);
+delay(delaytime);
+}
+
+void turnright(int delaytime)
+{
+digitalWrite (LEFT_REV, LOW);
+digitalWrite (RIGHT_REV, LOW);
+digitalWrite (LEFT_ENABLE, HIGH);
+digitalWrite (RIGHT_ENABLE, LOW);
+digitalWrite (LEFT_FWD, HIGH);
+digitalWrite (RIGHT_FWD, LOW);
+delay(delaytime);
+}
+void loop()
+{
+forward(2000);
+turnleft(850);
+forward(2000);
+turnleft(850);
+forward(2000);
+turnleft(850);
+forward(2000);
+turnleft(850);
+forward(2000);
+
+forward(2000);
+turnright(850);
+forward(2000);
+turnright(850);
+forward(2000);
+turnright(850);
+forward(2000);
+turnright(850);
+forward(2000);
+}
+
